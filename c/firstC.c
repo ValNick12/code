67eb3a7ebebde437dba7a41612f7937e2a5d59d2
ValNick12 <valchanov.nikolay.12@gmail.com>
2023-02-28 12:46:16 +0200

    First C code 1. Hello World 2. Value exchange

--- c/firstC.c
+++ c/firstC.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int a;
     int b;
-    printf("Enter a=");
-    scanf("%i\n", a);
-    printf("Enter b=");
-    scanf("%i", b);
+    printf("Enter a = ");
+    scanf("%i", &a);
+    printf("Enter b = ");
+    scanf("%i", &b);
     int ab = a + b;
-    
+    a = b;
+    b = ab - b;
+    printf("a = %i and b = %i\n", a, b);
     return 0;
 }
