f9c1813cf2f47198f3f895b6247be1bd186a250a
ValNick12 <valchanov.nikolay.12@gmail.com>
2023-02-28 12:45:39 +0200

    First C code 1. Hello World 2. Value exchange

---
+++ c/firstC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    printf("Enter a=");
+    scanf("%i\n", a);
+    printf("Enter b=");
+    scanf("%i", b);
+    int ab = a + b;
+    
+    return 0;
+}
